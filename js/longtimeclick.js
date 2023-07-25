9f3e232e46392289fa28f04601f76ceaf875f2e9
meow0222 <meow222.csgo@gmail.com>
2023-07-25 11:50:36 +0900

    Copy previous assignment.

---
+++ js/longtimeclick.js
@@ -0,0 +1,15 @@
+const clickBtn = document.getElementById('pin');
+const popupWrapper = document.getElementById('popup-wrapper');
+const close = document.getElementById('close');
+
+// ボタンをクリックしたときにポップアップを表示させる
+clickBtn.addEventListener('click', () => {
+  popupWrapper.style.display = "block";
+});
+
+// ポップアップの外側又は「x」のマークをクリックしたときポップアップを閉じる
+popupWrapper.addEventListener('click', e => {
+  if (e.target.id === popupWrapper.id || e.target.id === close.id) {
+    popupWrapper.style.display = 'none';
+  }
+});
